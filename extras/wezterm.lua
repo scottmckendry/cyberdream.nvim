c29427f0de49b288a663c8f090a4fd380f3a2228
letieu <letieu8@gmail.com>
2024-04-28 01:55:52 +0700

    feat: add config tab bar transparency

--- extras/wezterm.lua
+++ extras/wezterm.lua
@@ -15,4 +15,9 @@ return {
     ansi = { "#16181a", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
     brights = { "#3c4048", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
     indexed = { [16] = "#ffbd5e", [17] = "#ff6e5e" },
+
+    -- tab_bar = {
+    --   -- Make the tab ar transparent
+    --   background = "rgb(22, 24, 26 / 20%)",
+    -- },
 }
