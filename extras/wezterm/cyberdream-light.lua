eb3185d12135e6afb1bc0b49e3ad72f13261d97c
scottmckendry <scottmckendry@users.noreply.github.com>
2024-05-10 07:58:33 +0000

    chore: generate extras

---
+++ extras/wezterm/cyberdream-light.lua
@@ -0,0 +1,19 @@
+-- cyberdream theme for wezterm
+return {
+    foreground = "#16181a",
+    background = "#ffffff",
+
+    cursor_bg = "#16181a",
+    cursor_fg = "#ffffff",
+    cursor_border = "#16181a",
+
+    selection_fg = "#16181a",
+    selection_bg = "#acacac",
+
+    scrollbar_thumb = "#ffffff",
+    split = "#ffffff",
+
+    ansi = { "#ffffff", "#d11500", "#00ae0f", "#997b00", "#0057d1", "#a018ff", "#00afc0", "#16181a" },
+    brights = { "#acacac", "#d11500", "#00ae0f", "#997b00", "#0057d1", "#a018ff", "#00afc0", "#16181a" },
+    indexed = { [16] = "#d17c00", [17] = "#d11500" },
+}
