f98e9512edc0b0bbed649a8de671605c5e3cd21b
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-07-27 12:34:44 +1200

    refactor(cache): decouple logic

--- lua/cyberdream/cache.lua
+++ lua/cyberdream/cache.lua
@@ -4,37 +4,63 @@ local util = require("cyberdream.util")
 local M = {}
 local cache_file = vim.fn.stdpath("cache") .. "/cyberdream_cache.lua"
 
+local function gen_hl_opts_string(opts)
+    local opts_string = ""
+    for opt, value in pairs(opts) do
+        local value_str = ((type(value) == "string") and "'" .. value .. "'") or tostring(value)
+        opts_string = opts_string .. opt .. " = " .. value_str .. ", "
+    end
+    return opts_string
+end
+
+local function gen_highlights_string(highlights)
+    local highlights_string = ""
+    for group, opts in pairs(highlights) do
+        local opts_string = gen_hl_opts_string(opts)
+        highlights_string = highlights_string .. "vim.api.nvim_set_hl(0, '" .. group .. "', {" .. opts_string .. "})\n"
+    end
+    return highlights_string
+end
+
+local function gen_fillchars_string()
+    local fillchars_string = "vim.opt.fillchars:append({\n"
+    for key, value in pairs(vim.opt.fillchars:get()) do
+        fillchars_string = fillchars_string .. "  " .. key .. " = '" .. value .. "',\n"
+    end
+    fillchars_string = fillchars_string .. "})\n"
+    return fillchars_string
+end
+
+local function gen_terminal_colors_string()
+    local terminal_colors_string = ""
+    for i = 0, 15 do
+        terminal_colors_string = terminal_colors_string
+            .. "vim.g.terminal_color_"
+            .. i
+            .. " = '"
+            .. vim.g["terminal_color_" .. i]
+            .. "'\n"
+    end
+    return terminal_colors_string
+end
+
 --- build a cache file for a configured cyberdream theme
 --- @param theme table
 M.build = function(theme)
     local cache = io.open(cache_file, "w")
     if not cache then
         util.notify("Failed to open cache file", "error")
+        return
     end
 
-    for group, opts in pairs(theme.highlights) do
-        local opts_string = ""
-        for opt, value in pairs(opts) do
-            local value_str = ((type(value) == "string") and "'" .. value .. "'") or tostring(value)
-            opts_string = opts_string .. opt .. " = " .. value_str .. ", "
-        end
-        local line = "vim.api.nvim_set_hl(0, '" .. group .. "', {" .. opts_string .. "})"
-        cache:write(line .. "\n")
-    end
+    cache:write(gen_highlights_string(theme.highlights))
 
     if config.options.hide_fillchars then
-        cache:write([[
-vim.opt.fillchars:append({
-    horiz = " ",
-    horizup = " ",
-    horizdown = " ",
-    vert = " ",
-    vertleft = " ",
-    vertright = " ",
-    verthoriz = " ",
-    eob = " ",
-})
-        ]])
+        cache:write(gen_fillchars_string())
+    end
+
+    if config.options.terminal_colors then
+        cache:write(gen_terminal_colors_string())
     end
 
     cache:write("vim.g.colors_name = 'cyberdream'\n")
