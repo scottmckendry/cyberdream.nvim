a36ef6f05ee1e1ea8c29c8ff3bee293a5bd8e7b7
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-08-19 12:43:48 +1200

    fix(telescope): 'flat' style look closer to readme screenshots

--- lua/cyberdream/extensions/telescope.lua
+++ lua/cyberdream/extensions/telescope.lua
@@ -20,6 +20,7 @@ function M.get(opts, t)
         TelescopePreviewTitle = { fg = t.magenta },
         TelescopeSelection = { bg = t.bgHighlight },
         TelescopePromptCounter = { fg = t.pink },
+        TelescopeMatching = { fg = t.cyan },
     }
 
     if borderless_telescope then
@@ -27,22 +28,24 @@ function M.get(opts, t)
         highlights.TelescopeNormal = { bg = t.bgAlt }
         highlights.TelescopePreviewBorder = { fg = t.bgAlt, bg = t.bgAlt }
         highlights.TelescopePreviewNormal = { bg = t.bgAlt }
+        highlights.TelescopeResultsTitle = { fg = t.bgAlt, bg = t.cyan }
+        highlights.TelescopeResultsBorder = { fg = t.bgAlt, bg = t.bgAlt }
+        highlights.TelescopeResultsNormal = { bg = t.bgAlt }
         if telescope_style == "nvchad" then
             highlights.TelescopePreviewTitle = { fg = t.bgAlt, bg = t.green, bold = true }
             highlights.TelescopePromptBorder = { fg = t.bgHighlight, bg = t.bgHighlight }
             highlights.TelescopePromptNormal = { fg = t.fg, bg = t.bgHighlight }
             highlights.TelescopePromptPrefix = { fg = t.red, bg = t.bgHighlight }
             highlights.TelescopePromptTitle = { fg = t.bgAlt, bg = t.red, bold = true }
-        else
-            highlights.TelescopePreviewTitle = { fg = t.bgAlt, bg = t.green }
+        elseif telescope_style == "flat" then
+            highlights.TelescopePreviewTitle = { fg = t.bgAlt, bg = t.green, bold = true }
             highlights.TelescopePromptBorder = { fg = t.bgAlt, bg = t.bgAlt }
             highlights.TelescopePromptNormal = { fg = t.fg, bg = t.bgAlt }
-            highlights.TelescopePromptPrefix = { fg = t.red, bg = t.bgAlt }
-            highlights.TelescopePromptTitle = { fg = t.bgAlt, bg = t.red }
+            highlights.TelescopePromptPrefix = { fg = t.blue, bg = t.bgAlt }
+            highlights.TelescopePromptCounter = { fg = t.cyan, bg = t.bgAlt }
+            highlights.TelescopePromptTitle = { fg = t.bgAlt, bg = t.blue, bold = true }
+            highlights.TelescopeResultsTitle = { fg = t.blue, bg = t.bgAlt, bold = true }
         end
-        highlights.TelescopeResultsBorder = { fg = t.bgAlt, bg = t.bgAlt }
-        highlights.TelescopeResultsNormal = { bg = t.bgAlt }
-        highlights.TelescopeResultsTitle = { fg = t.bgAlt, bg = t.cyan }
     end
 
     return highlights
