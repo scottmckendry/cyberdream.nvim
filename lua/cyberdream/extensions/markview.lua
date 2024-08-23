4f402704f52acb53306d63f7eae1003871f723f2
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-08-24 08:38:33 +1200

    feat(extensions): update markview.nvim highlight groups
    fixes #127

--- lua/cyberdream/extensions/markview.lua
+++ lua/cyberdream/extensions/markview.lua
@@ -7,23 +7,34 @@ local util = require("cyberdream.util")
 function M.get(opts, t)
     opts = opts or {}
     local highlights = {
-        MarkviewCol1 = { fg = t.orange, bg = util.blend(t.bg_solid, t.orange, 0.8), bold = true },
-        MarkviewCol2 = { fg = t.cyan, bg = util.blend(t.bg_solid, t.cyan, 0.8), bold = true },
-        MarkviewCol3 = { fg = t.blue, bg = util.blend(t.bg_solid, t.blue, 0.8), bold = true },
-        MarkviewCol4 = { fg = t.purple, bg = util.blend(t.bg_solid, t.purple, 0.8), bold = true },
-        MarkviewCol5 = { fg = t.magenta, bg = util.blend(t.bg_solid, t.magenta, 0.8), bold = true },
-        MarkviewCol6 = { fg = t.green, bg = util.blend(t.bg_solid, t.green, 0.8), bold = true },
+        MarkviewHeading1 = { fg = t.orange, bg = util.blend(t.bg_solid, t.orange, 0.8), bold = true },
+        MarkviewHeading2 = { fg = t.cyan, bg = util.blend(t.bg_solid, t.cyan, 0.8), bold = true },
+        MarkviewHeading3 = { fg = t.blue, bg = util.blend(t.bg_solid, t.blue, 0.8), bold = true },
+        MarkviewHeading4 = { fg = t.purple, bg = util.blend(t.bg_solid, t.purple, 0.8), bold = true },
+        MarkviewHeading5 = { fg = t.magenta, bg = util.blend(t.bg_solid, t.magenta, 0.8), bold = true },
+        MarkviewHeading6 = { fg = t.green, bg = util.blend(t.bg_solid, t.green, 0.8), bold = true },
 
-        MarkviewLayer = { bg = util.blend(t.bgAlt, t.bgHighlight, 0.8) },
-        MarkviewLayer2 = { bg = util.blend(t.bgAlt, t.bgHighlight, 0.8) },
+        MarkviewHeading1Sign = { fg = t.orange },
+        MarkviewHeading2Sign = { fg = t.cyan },
+        MarkviewHeading3Sign = { fg = t.blue },
+        MarkviewHeading4Sign = { fg = t.purple },
+        MarkviewHeading5Sign = { fg = t.magenta },
+        MarkviewHeading6Sign = { fg = t.green },
 
-        MarkviewCol1Fg = { fg = t.red }, -- danger, bug, caution
-        MarkviewCol2Fg = { fg = t.orange }, -- question, warning
-        MarkviewCol3Fg = { fg = t.yellow }, -- important, custom
-        MarkviewCol4Fg = { fg = t.cyan }, -- success, tip
-        MarkviewCol5Fg = { fg = t.blue }, -- abstract, todo, note
-        MarkviewCol6Fg = { fg = t.pink }, -- example
-        MarkviewCol7Fg = { fg = t.grey }, -- quote
+        MarkviewBlockQuoteDefault = { link = "Comment" },
+        MarkviewBlockQuoteOk = { fg = t.green },
+        MarkviewBlockQuoteWarn = { fg = t.yellow },
+        MarkviewBlockQuoteError = { fg = t.red },
+        MarkviewBlockQuoteNote = { fg = t.blue },
+        MarkviewBlockQuoteSpecial = { fg = t.cyan },
+
+        MarkviewCode = { bg = util.blend(t.bgAlt, t.bgHighlight, 0.8) },
+        MarkviewInlineCode = { fg = t.pink, bg = util.blend(t.bgAlt, t.bgHighlight, 0.8) },
+
+        MarkviewTableBorder = { fg = t.bgHighlight },
+        MarkviewTableAlignLeft = { fg = t.green },
+        MarkviewTableAlignCenter = { fg = t.blue },
+        MarkviewTableAlignRight = { fg = t.purple },
     }
 
     for i = 1, 10 do
