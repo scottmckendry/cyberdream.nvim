6985950962b1c89bc2d1b6c23900dcd7b6eb245e
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-10-20 10:41:25 +1300

    fix(extras): contrast in counter status component
    along with quite a large refactor of the config file. Much easier to
    read now. Resolves #142.

--- lua/cyberdream/extra/yazi.lua
+++ lua/cyberdream/extra/yazi.lua
@@ -7,245 +7,94 @@ local M = {}
 --- @param variant string: Variation of the colorscheme to use.
 function M.generate(variant)
     local template = [==[
-[manager] 
-    # tmTheme files can be found here: https://github.com/scottmckendry/cyberdream.nvim/tree/main/extras/textmate 
-    syntect_theme = "../bat/themes/cyberdream.tmTheme"
-    [manager.border_style]
-        fg = "${grey}"
+[manager]
+# tmTheme files can be found here: https://github.com/scottmckendry/cyberdream.nvim/tree/main/extras/textmate 
+syntect_theme = "../bat/themes/cyberdream.tmTheme"
+border_style = { fg = "${bgHighlight}" }
+cwd = { fg = "${cyan}" }
+find_keyword = { bold = true, fg = "${green}" }
+find_position = { fg = "${fg}" }
+hovered = { bg = "${grey}", bold = true, fg = "${fg}" }
+marker_copied = { bg = "${yellow}", fg = "${yellow}" }
+marker_cut = { bg = "${red}", fg = "${red}" }
+marker_selected = { bg = "${bgHighlight}", fg = "${green}" }
+preview_hovered = { bg = "${bgHighlight}", bold = true, fg = "${fg}" }
+tab_active = { bg = "${blue}", fg = "${bg}" }
+tab_inactive = { bg = "${bgHighlight}", fg = "${fg}" }
 
-    [manager.cwd]
-        fg = "${cyan}"
-
-    [manager.find_keyword]
-        bold = true
-        fg = "${green}"
-
-    [manager.find_position]
-        fg = "${fg}"
-
-    [manager.hovered]
-        bg = "${grey}"
-        bold = true
-        fg = "${fg}"
-
-    [manager.marker_copied]
-        bg = "${yellow}"
-        fg = "${yellow}"
-
-    [manager.marker_cut]
-        bg = "${red}"
-        fg = "${red}"
-
-    [manager.marker_selected]
-        bg = "${green}"
-        fg = "${green}"
-
-    [manager.preview_hovered]
-        bg = "${grey}"
-        bold = true
-        fg = "${fg}"
-
-    [manager.tab_active]
-        bg = "${blue}"
-        fg = "${bg}"
-
-    [manager.tab_inactive]
-        bg = "${bgHighlight}"
-        fg = "${fg}"
+count_selected = { bg = "${green}", fg = "${bg}" }
+count_copied = { bg = "${yellow}", fg = "${bg}" }
+count_cut = { bg = "${red}", fg = "${bg}" }
 
 [completion]
-    [completion.active]
-        bg = "${grey}"
-        fg = "${purple}"
+active = { bg = "${grey}", fg = "${purple}" }
+border = { fg = "${blue}" }
+inactive = { fg = "${fg}" }
 
-    [completion.border]
-        fg = "${blue}"
-
-    [completion.inactive]
-        fg = "${fg}"
-
-[[filetype.rules]]
-    fg = "${cyan}"
-    mime = "image/*"
-
-[[filetype.rules]]
-    fg = "${yellow}"
-    mime = "video/*"
-
-[[filetype.rules]]
-    fg = "${yellow}"
-    mime = "audio/*"
-
-[[filetype.rules]]
-    fg = "${purple}"
-    mime = "application/zip"
-
-[[filetype.rules]]
-    fg = "${purple}"
-    mime = "application/gzip"
-
-[[filetype.rules]]
-    fg = "${purple}"
-    mime = "application/x-tar"
-
-[[filetype.rules]]
-    fg = "${purple}"
-    mime = "application/x-bzip"
-
-[[filetype.rules]]
-    fg = "${purple}"
-    mime = "application/x-bzip2"
-
-[[filetype.rules]]
-    fg = "${purple}"
-    mime = "application/x-7z-compressed"
-
-[[filetype.rules]]
-    fg = "${purple}"
-    mime = "application/x-rar"
-
-[[filetype.rules]]
-    fg = "${purple}"
-    mime = "application/xz"
-
-[[filetype.rules]]
-    fg = "${green}"
-    mime = "application/doc"
-
-[[filetype.rules]]
-    fg = "${green}"
-    mime = "application/pdf"
-
-[[filetype.rules]]
-    fg = "${green}"
-    mime = "application/rtf"
-
-[[filetype.rules]]
-    fg = "${green}"
-    mime = "application/vnd.*"
-
-[[filetype.rules]]
-    bold = true
-    fg = "${blue}"
-    mime = "inode/directory"
-
-[[filetype.rules]]
-    fg = "${fg}"
-    mime = "*"
+[filetype]
+rules = [
+    { fg = "${cyan}", mime = "image/*" },
+    { fg = "${yellow}", mime = "video/*" },
+    { fg = "${yellow}", mime = "audio/*" },
+    { fg = "${purple}", mime = "application/zip" },
+    { fg = "${purple}", mime = "application/gzip" },
+    { fg = "${purple}", mime = "application/x-tar" },
+    { fg = "${purple}", mime = "application/x-bzip" },
+    { fg = "${purple}", mime = "application/x-bzip2" },
+    { fg = "${purple}", mime = "application/x-7z-compressed" },
+    { fg = "${purple}", mime = "application/x-rar" },
+    { fg = "${purple}", mime = "application/xz" },
+    { fg = "${green}", mime = "application/doc" },
+    { fg = "${green}", mime = "application/pdf" },
+    { fg = "${green}", mime = "application/rtf" },
+    { fg = "${green}", mime = "application/vnd.*" },
+    { bold = true, fg = "${blue}", mime = "inode/directory" },
+    { fg = "${fg}", mime = "*" }
+]
 
 [help]
-    [help.desc]
-        fg = "${fg}"
-
-    [help.footer]
-        fg = "${fg}"
-
-    [help.hovered]
-        bg = "${grey}"
-        fg = "${fg}"
-
-    [help.on]
-        fg = "${purple}"
-
-    [help.run]
-        fg = "${cyan}"
+desc = { fg = "${fg}" }
+footer = { fg = "${fg}" }
+hovered = { bg = "${grey}", fg = "${fg}" }
+on = { fg = "${purple}" }
+run = { fg = "${cyan}" }
 
 [input]
-    [input.border]
-        fg = "${blue}"
-
-    [input.selected]
-        bg = "${grey}"
-
-    [input.title]
-        fg = "${fg}"
-
-    [input.value]
-        fg = "${fg}"
+border = { fg = "${blue}" }
+selected = { bg = "${grey}" }
+title = { fg = "${fg}" }
+value = { fg = "${fg}" }
 
 [select]
-    [select.active]
-        fg = "${purple}"
-
-    [select.border]
-        fg = "${blue}"
-
-    [select.inactive]
-        fg = "${fg}"
+active = { fg = "${purple}" }
+border = { fg = "${blue}" }
+inactive = { fg = "${fg}" }
 
 [status]
-    [status.mode_normal]
-        bg = "${blue}"
-        bold = true
-        fg = "${bg}"
-
-    [status.mode_select]
-        bg = "${green}"
-        bold = true
-        fg = "${bg}"
-
-    [status.mode_unset]
-        bg = "${magenta}"
-        bold = true
-        fg = "${bg}"
-
-    [status.permissions_r]
-        fg = "${yellow}"
-
-    [status.permissions_s]
-        fg = "${cyan}"
-
-    [status.permissions_t]
-        fg = "${blue}"
-
-    [status.permissions_w]
-        fg = "${red}"
-
-    [status.permissions_x]
-        fg = "${green}"
-
-    [status.progress_error]
-        bg = "${bg}"
-        fg = "${red}"
-
-    [status.progress_label]
-        bg = "${bg}"
-        fg = "${fg}"
-
-    [status.progress_normal]
-        bg = "${bg}"
-        fg = "${fg}"
-
-    [status.separator_style]
-        bg = "${bgHighlight}"
-        fg = "${bgHighlight}"
+mode_normal = { bg = "${blue}", bold = true, fg = "${bg}" }
+mode_select = { bg = "${green}", bold = true, fg = "${bg}" }
+mode_unset = { bg = "${magenta}", bold = true, fg = "${bg}" }
+permissions_r = { fg = "${yellow}" }
+permissions_s = { fg = "${cyan}" }
+permissions_t = { fg = "${blue}" }
+permissions_w = { fg = "${red}" }
+permissions_x = { fg = "${green}" }
+progress_error = { bg = "${bg}", fg = "${red}" }
+progress_label = { bg = "${bg}", fg = "${fg}" }
+progress_normal = { bg = "${bg}", fg = "${fg}" }
+separator_style = { bg = "${bgHighlight}", fg = "${bgHighlight}" }
 
 [tasks]
-    [tasks.border]
-        fg = "${blue}"
-
-    [tasks.hovered]
-        bg = "${grey}"
-        fg = "${fg}"
-
-    [tasks.title]
-        fg = "${fg}"
+border = { fg = "${blue}" }
+hovered = { bg = "${grey}", fg = "${fg}" }
+title = { fg = "${fg}" }
 
 [which]
-    [which.cand]
-        fg = "${cyan}"
-
-    [which.desc]
-        fg = "${fg}"
-
-    [which.mask]
-        bg = "${bgHighlight}"
-
-    [which.rest]
-        fg = "${magenta}"
-
-    [which.separator_style]
-        fg = "${grey}"
+cand = { fg = "${cyan}" }
+desc = { fg = "${fg}" }
+mask = { bg = "${bgHighlight}" }
+rest = { fg = "${magenta}" }
+separator_style = { fg = "${grey}" }
 ]==]
 
     return util.parse_extra_template(template, colors[variant])
