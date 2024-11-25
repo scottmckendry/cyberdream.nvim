534527bbdefbaccf1e99c213e98d0871e35cc596
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-11-26 08:52:31 +1300

    fix(extensions): add missing whichkey highlights
    related #147

--- lua/cyberdream/extensions/whichkey.lua
+++ lua/cyberdream/extensions/whichkey.lua
@@ -1,4 +1,5 @@
 local M = {}
+local util = require("cyberdream.util")
 
 --- Get extension configuration
 --- @param opts Config
@@ -7,11 +8,25 @@ function M.get(opts, t)
     opts = opts or {}
     local highlights = {
         WhichKey = { fg = t.cyan },
+        WhichKeyNormal = { link = "NormalFloat" },
+        WhichKeyBorder = { link = "FloatBorder" },
+        WhichKeyTitle = { link = "FloatTitle" },
         WhichKeyGroup = { fg = t.blue },
         WhichKeyDesc = { fg = t.pink },
-        WhichKeySeperator = { fg = t.bg },
+        WhichKeySeperator = { fg = t.grey },
         WhichKeyFloat = { bg = t.bg },
         WhichKeyValue = { fg = t.blue },
+
+        WhichKeyIcon = { fg = t.blue },
+        WhichKeyIconAzure = { fg = util.blend(t.cyan, t.fg, 0.3) },
+        WhichKeyIconBlue = { fg = t.blue },
+        WhickKeyIconCyan = { fg = t.cyan },
+        WhichKeyIconGreen = { fg = t.green },
+        WhichKeyIconGrey = { fg = t.grey },
+        WhichKeyIconOrange = { fg = t.orange },
+        WhichKeyIconPurple = { fg = t.purple },
+        WhichKeyIconRed = { fg = t.red },
+        WhichKeyIconYellow = { fg = t.yellow },
     }
 
     return highlights
