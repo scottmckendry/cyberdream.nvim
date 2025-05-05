ab98c12afdb72231f5a8be7aaa25f8c98c203159
Cupsa Darius <cupsadarius@gmail.com>
2025-05-05 17:16:42 +0300

    add tron base colorscheme

--- lua/cyberdream/colors.lua
+++ lua/cyberdream/colors.lua
@@ -54,4 +54,21 @@ M.light = {
     purple = "#a018ff",
 }
 
+M.tron = {
+    bg = "#16181a",
+    bg_alt = "#1e2124",
+    bg_highlight = "#3c4048",
+    fg = "#ffffff",
+    grey = "#7b8496",
+    blue = "#5ea1ff",
+    green = "#5eff6c",
+    cyan = "#5ef1ff",
+    red = "#ff6e5e",
+    yellow = "#f1ff5e",
+    magenta = "#ff5ef1",
+    pink = "#ff5ea0",
+    orange = "#ffbd5e",
+    purple = "#bd5eff",
+}
+
 return M
