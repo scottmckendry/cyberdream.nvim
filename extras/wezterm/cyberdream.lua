c8d9283f54bc133cb1f3aa74a48e0073fe16ad1d
Sarah H. McGrath <sarah@shmcgrath.com>
2026-01-13 23:51:55 -0500

    Standardize index 17 colors to pink
    Changed all colros in index 17 to pink, they were red in WezTerm
    and magenta in alacritty

--- extras/wezterm/cyberdream.lua
+++ extras/wezterm/cyberdream.lua
@@ -15,5 +15,5 @@ return {
 
     ansi = { "#16181a", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
     brights = { "#3c4048", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
-    indexed = { [16] = "#ffbd5e", [17] = "#ff6e5e" },
+    indexed = { [16] = "#ffbd5e", [17] = "#ff5ea0" },
 }
