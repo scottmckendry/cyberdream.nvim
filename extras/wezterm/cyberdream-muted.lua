d0e6e837b0248e3507f3a295deb4661221610e78
scottmckendry <39483124+scottmckendry@users.noreply.github.com>
2026-08-01 22:13:21 +0000

    chore: generate extras

---
+++ extras/wezterm/cyberdream-muted.lua
@@ -0,0 +1,19 @@
+-- cyberdream theme for wezterm
+return {
+    foreground = "#ffffff",
+    background = "#16181a",
+
+    cursor_bg = "#ffffff",
+    cursor_fg = "#16181a",
+    cursor_border = "#ffffff",
+
+    selection_fg = "#ffffff",
+    selection_bg = "#3c4048",
+
+    scrollbar_thumb = "#16181a",
+    split = "#16181a",
+
+    ansi = { "#16181a", "#e8796f", "#7ee088", "#e3c875", "#78a8e8", "#b07ee8", "#7ed8e8", "#ffffff" },
+    brights = { "#3c4048", "#e8796f", "#7ee088", "#e3c875", "#78a8e8", "#b07ee8", "#7ed8e8", "#ffffff" },
+    indexed = { [16] = "#e3a875", [17] = "#e8796f" },
+}
